6a291a6e3d5711d6036481ed4f7c8abf3829171d
yedeguo <>
2018-12-20 17:07:52 +0800

    添加一些注释

---
+++ src/main/java/com/ydg/myproject/constant/Constant.java
@@ -0,0 +1,16 @@
+package com.ydg.myproject.constant;
+
+/**
+ * 常量维护接口
+ *
+ * @author ydg
+ * @Company qcsz
+ * @date 2018-12-20
+ * @description
+ */
+public interface Constant {
+    /**
+     * 线程for循环的次数
+     */
+    Integer THREAD_RUN_TIME = 100;
+}
--- src/main/java/com/ydg/myproject/service/impl/ThreadServiceImpl.java
+++ src/main/java/com/ydg/myproject/service/impl/ThreadServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ydg.myproject.service.impl;
 
+import com.ydg.myproject.constant.Constant;
 import com.ydg.myproject.service.ThreadService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,24 +26,27 @@ public class ThreadServiceImpl implements ThreadService {
     @Override
     public void runManyThread() {
         log.info("当前的线程是：" + Thread.currentThread().getName());
-        //        for (int i = 0; i < 2; i++) {
         threadComponent.thread1();
         threadComponent.thread2();
-        //        }
     }
 
+    /**
+     * 具体的需要执行的任务类
+     * 不能this.@Async方法这样会不生效
+     * 索引将@Async方法抽离到另一个类中，这样就能生效了
+     */
     @Component
     class ThreadComponent {
         @Async
         public void thread1() {
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < Constant.THREAD_RUN_TIME; i++) {
                 log.info("thread1,当前执行的线程是：" + Thread.currentThread().getName() + "执行到：" + i + "条记录");
             }
         }
 
         @Async
         public void thread2() {
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < Constant.THREAD_RUN_TIME; i++) {
                 log.info("thread2,当前执行的线程是：" + Thread.currentThread().getName() + "执行到：" + i + "条记录");
             }
         }
