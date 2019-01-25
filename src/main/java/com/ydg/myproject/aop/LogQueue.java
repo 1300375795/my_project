508df8848f997d45b4feb44523ffc27961f7b58e
yedeguo <yedeguo@yedeguodeMacBook-Pro.local>
2019-01-25 18:00:14 +0800

    测试

--- src/main/java/com/ydg/myproject/aop/LogQueue.java
+++ src/main/java/com/ydg/myproject/aop/LogQueue.java
@@ -1,26 +1,26 @@
-package com.ydg.myproject.aop;
-
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
-import org.springframework.stereotype.Component;
-
-/**
- * @author ydg
- * @Company qcsz
- * @date 2018-12-15
- * @description
- */
-@Component
-public class LogQueue {
-    private BlockingQueue<Object> logQueue = new LinkedBlockingQueue<>();
-
-    public void add(Object log) {
-        logQueue.add(log);
-    }
-
-    public Object poll() throws InterruptedException {
-        return logQueue.poll(5, TimeUnit.SECONDS);
-    }
-
-}
+//package com.ydg.myproject.aop;
+//
+//import java.util.concurrent.BlockingQueue;
+//import java.util.concurrent.LinkedBlockingQueue;
+//import java.util.concurrent.TimeUnit;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author ydg
+// * @Company qcsz
+// * @date 2018-12-15
+// * @description
+// */
+//@Component
+//public class LogQueue {
+//    private BlockingQueue<Object> logQueue = new LinkedBlockingQueue<>();
+//
+//    public void add(Object log) {
+//        logQueue.add(log);
+//    }
+//
+//    public Object poll() throws InterruptedException {
+//        return logQueue.poll(5, TimeUnit.SECONDS);
+//    }
+//
+//}
